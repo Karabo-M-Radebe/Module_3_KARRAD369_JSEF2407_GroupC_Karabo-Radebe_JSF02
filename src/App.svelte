3fa1e468dd40a6affb8c6065a39699fb0caac9de
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-07-30 14:40:19 +0200

    added a loading state for the cards in the 'LoadingCard.svelte' file with the default svg cards then created the product cards by use of api data and fetching in the 'CardView.svelte' file then I called the components in the 'App.svelte' file

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import { Router, Route,} from 'svelte-routing';
   import Navbar from './lib/Navbar.svelte';
-  import Home from './lib/Home.svelte';
+  import Home from './pages/Home.svelte';
 </script>
 
 <Navbar />
